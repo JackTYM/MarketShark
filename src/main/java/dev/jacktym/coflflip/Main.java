db2119f124623ef1958ceb36294895e2ab624e8f
JackTYM <jacksonkyarger@gmail.com>
2024-05-23 11:21:01 -0700

    Set default Locale to en-US

--- src/main/java/dev/jacktym/coflflip/Main.java
+++ src/main/java/dev/jacktym/coflflip/Main.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @Mod(modid = "coflflip", name = "CoflFlip", version = "1.0.0")
 public class Main {
@@ -34,5 +35,7 @@ public class Main {
         registry.add(new RealtimeEventRegistry());
         registry.add(new AutoClaimSold());
         registry.forEach(MinecraftForge.EVENT_BUS::register);
+
+        Locale.setDefault(new Locale("en", "US"));
     }
 }
