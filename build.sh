9ad0db7455915a320e13db83b336200c2ce63351
Jackson Yarger <50038825+JackTYM@users.noreply.github.com>
2024-06-12 21:53:46 -0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Running Gradlew Build Hammerhead"
+./gradlew buildHammerhead
+
+echo "Running Gradlew Build Wobbegong"
+./gradlew buildWobbegong
+
+echo "Running Gradlew Build GreatWhite"
+./gradlew buildGreatWhite
+
+echo "Running Gradlew Build Megalodon"
+./gradlew buildMegalodon
